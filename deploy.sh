72516e75b981ba60c61cab1248a2ce4ce770ee85
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-04-22 19:18:22 -0300

    update

--- deploy.sh
+++ deploy.sh
@@ -25,4 +25,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force "https://${GITHUB_TOKEN}@${GITHUB_REPO}" master:gh-pages

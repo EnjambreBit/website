1d58f16bb084429f6e815d313a1959b90ce31e11
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-04-23 22:43:51 -0300

    agregando fix para hacer scroll en los links y mejorando slider.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,5 +10,10 @@ module.exports = function(defaults) {
   app.import("vendor/bootstrap.css");
   app.import("vendor/enjambre-red.css");
 
+  app.import("vendor/jquery.themepunch.plugins.min.js");
+  app.import("vendor/jquery.themepunch.revolution.min.js");
+  app.import("vendor/jquery.localScroll.js");
+  
+
   return app.toTree();
 };

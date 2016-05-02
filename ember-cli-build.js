016036fd236a1be9eff3cf2d5796a9b6f482100e
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-05-01 23:51:01 -0300

    Creando componente para representar las secciones.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,8 +4,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-     fingerprint: {
-	enabled: false,
+    fingerprint: {
+	    enabled: false,
      }
   });
 

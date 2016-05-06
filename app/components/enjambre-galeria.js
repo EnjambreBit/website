6b5ad3ae5512d9aa78a570435e1d42b9651cf165
digitalh <digital.h@gmail.com>
2016-05-06 20:06:08 -0300

    Codigo de inicio para galeria

--- app/components/enjambre-galeria.js
+++ app/components/enjambre-galeria.js
@@ -3,7 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     didInsertElement(){
     var $container = $('.lightbox-wrapper .items');
-
+    $container.imagesLoaded(function () {
+        $container.isotope({
+            itemSelector: '.lightbox-wrapper .item',
+            layoutMode: 'fitRows'
+        });
+    });
     $('.lightbox-wrapper .filter li a').click(function () {
 
         $('.lightbox-wrapper .filter li a').removeClass('active');

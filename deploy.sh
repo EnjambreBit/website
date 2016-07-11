da7efa1e3e3dd1d26a3a0675b3af03976c8724f8
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-07-11 10:39:34 -0300

    Commit de prueba

--- deploy.sh
+++ deploy.sh
@@ -33,6 +33,6 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force "https://hugoruscitti:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}" master:gh-pages
+git push --force "https://hugoruscitti:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}" master:gh-pages  > /dev/null 2>&1
 
 echo "Deploy complete: http://enjambrebit.github.io/website "

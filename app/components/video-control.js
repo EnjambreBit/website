431aedc214096c007a85794fe9fd9c7ef67da670
Iván Hoffmann <ihoffmann@enjambrebit.com.ar>
2017-03-01 14:46:36 -0300

    Reversionando video placeholder y responsive stuff

--- app/components/video-control.js
+++ app/components/video-control.js
@@ -1,33 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    didInsertElement(){
-		/* Video inicial */
-		$("#welcome-video").bind("ended", function() {
-			$("#home-placeholder").hide();
-			$("#welcome-video-container").fadeOut("slow");
-		});
+  didInsertElement() {
+    /* Video inicial */
+    // $("#welcome-video").bind("ended", function() {
+    // 	$("#home-placeholder").hide();
+    // 	$("#welcome-video-container").fadeOut("slow");
+    // });
+    $(".ebit-home").hide();
+    $("#video-quienes-somos").bind("ended", function() {
+      $("#welcome-video-container").fadeOut("slow");
+      $(".ebit-home").fadeIn("slow");
+    });
 
-		/* Quienes somos home */
-		$("#quienes-somos-video-home").bind("play", function() {
-			$("#quienes-somos-home-link").fadeOut("fast");
-		});
-		$("#quienes-somos-video-home").bind("ended", function() {
-			$("#quienes-somos-home-link").fadeIn("slow");
-		});
-		$("#quienes-somos-video-home").bind("pause", function() {
-			$("#quienes-somos-home-link").fadeIn("slow");
-		});
+    /* Quienes somos home */
+    // $("#quienes-somos-video-home").bind("play", function() {
+    // 	$("#quienes-somos-home-link").fadeOut("fast");
+    // });
+    // $("#quienes-somos-video-home").bind("ended", function() {
+    // 	$("#quienes-somos-home-link").fadeIn("slow");
+    // });
+    // $("#quienes-somos-video-home").bind("pause", function() {
+    // 	$("#quienes-somos-home-link").fadeIn("slow");
+    // });
 
-		/* Que hacemos home */
-		$("#que-hacemos-video-home").bind("play", function() {
-			$("#que-hacemos-home-link").fadeOut("fast");
-		});
-		$("#que-hacemos-video-home").bind("ended", function() {
-			$("#que-hacemos-home-link").fadeIn("slow");
-		});
-		$("#que-hacemos-video-home").bind("pause", function() {
-			$("#que-hacemos-home-link").fadeIn("slow");
-		});
-	}
+    /* Que hacemos home */
+    // $("#que-hacemos-video-home").bind("play", function() {
+    // 	$("#que-hacemos-home-link").fadeOut("fast");
+    // });
+    // $("#que-hacemos-video-home").bind("ended", function() {
+    // 	$("#que-hacemos-home-link").fadeIn("slow");
+    // });
+    // $("#que-hacemos-video-home").bind("pause", function() {
+    // 	$("#que-hacemos-home-link").fadeIn("slow");
+    // });
+  }
 });
